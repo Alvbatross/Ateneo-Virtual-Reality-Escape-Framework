1c2dd3f441606710ca34e61faa470320772b5f00
Alvbatross <alvaropato54@gmail.com>
2025-01-14 13:02:52 +0800

    Started Testing Project

---
+++ src/xr_player.gd
@@ -0,0 +1,9 @@
+extends Node3D
+
+
+func _on_xr_controller_3d_left_button_pressed(name: String) -> void:
+	print(name)
+
+
+func _on_xr_controller_3d_right_button_pressed(name: String) -> void:
+	print(name)
