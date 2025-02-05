d39b615d920dc5720b99fda5504b446d1c309cf3
Alvbatross <alvaropato54@gmail.com>
2025-02-05 16:16:12 +0800

    Added old OIS Twist. TODO: Work on the fixes to twist

---
+++ addons/ateneo-virtual-reality-escape/object-interaction-system/ois-receiver/ois_twist_receiver.gd
@@ -0,0 +1,39 @@
+@tool
+class_name OISTwistReceiver
+extends OISReceiverComponent
+
+var interacting_inital_angle
+var delta_angle_buffer
+var past_progress = 0
+
+
+func initialize_action_vars():
+	interacting_inital_angle = interacting_object.basis.get_euler().z
+	past_progress = total_progress
+	delta_angle_buffer = 0
+
+
+func action_ongoing(delta: float) -> void:
+	var interacting_current_angle = interacting_object.basis.get_euler().z
+	# print(name + ": " +  interacting_object.name + " " + str(interacting_inital_angle) + " " + str(interacting_current_angle))
+	
+	var delta_angle = interacting_current_angle-interacting_inital_angle
+	# get shortest angle between two angles
+	if (delta_angle > PI || delta_angle < -PI):
+		delta_angle = sign(delta_angle)*(abs(delta_angle)-(2*PI))
+		
+	var current_progress = delta_angle_buffer + delta_angle
+	
+	# calculate twisting action when rotation is above 180/below -180
+	if delta_angle >= 3:
+		delta_angle_buffer += 3
+		interacting_inital_angle = interacting_current_angle
+	elif delta_angle <= -3:
+		delta_angle_buffer -= 3
+		interacting_inital_angle = interacting_current_angle
+	
+	total_progress = past_progress + (current_progress * rate);
+	print(total_progress)
+	super(delta)
+
+	
