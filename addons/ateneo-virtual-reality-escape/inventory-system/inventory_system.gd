dc826de44037a0d65649891cde51d4e88234f209
Alvbatross <alvaropato54@gmail.com>
2025-02-21 12:38:07 +0800

    Added starting scripts for event and inventory

---
+++ addons/ateneo-virtual-reality-escape/inventory-system/inventory_system.gd
@@ -0,0 +1,13 @@
+class_name InventorySystem
+extends Node3D
+
+# Place all common Inventory System Stuff in here. Common to shelf type and Wrist type inventories. 
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
