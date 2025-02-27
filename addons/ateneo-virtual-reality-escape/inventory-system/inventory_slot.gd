2256ae212a10fef93f5380b60190e56425c56078
Colleen Garcia <colleen.garcia@student.ateneo.edu>
2025-02-27 10:14:26 +0800

    Fixed Inventory Slot Errors after reloading

--- addons/ateneo-virtual-reality-escape/inventory-system/inventory_slot.gd
+++ addons/ateneo-virtual-reality-escape/inventory-system/inventory_slot.gd
@@ -8,11 +8,12 @@ signal current_object_in_slot
 @export var snap_zone_radius : float = 0.2
 @export var default_object : NodePath
 @export var group_required : String
-@export var slot_material_override = preload("res://addons/ateneo-virtual-reality-escape/inventory-system/inventory_slot_shader_a.tres")
+@export var slot_material_override = preload("res://addons/ateneo-virtual-reality-escape/inventory-system/misc-resources/inventory_slot_shader_a.tres")
 
 
-@onready var mesh_shape_test := MeshInstance3D.new()
-@onready var snap_zone_scene := preload("res://addons/godot-xr-tools/objects/snap_zone.tscn")
+var snap_zone_mesh := MeshInstance3D.new()
+var mesh_shape := SphereMesh.new()
+var snap_zone_scene := preload("res://addons/godot-xr-tools/objects/snap_zone.tscn")
 
 var snap_zone : XRToolsSnapZone
 var current_object : Node3D
@@ -26,19 +27,18 @@ func _ready() -> void:
 		add_child(snap_zone)
 		snap_zone.owner = get_tree().edited_scene_root
 		
-		var new_test_mesh = SphereMesh.new()
-		new_test_mesh.radius = snap_zone_radius
-		new_test_mesh.height = snap_zone_radius * 2
-		mesh_shape_test.mesh = new_test_mesh
-		mesh_shape_test.set_surface_override_material(0,slot_material_override)
-		mesh_shape_test.name = "MeshInstance3D"
-		add_child(mesh_shape_test)
-		mesh_shape_test.owner = get_tree().edited_scene_root
+		mesh_shape.radius = snap_zone_radius
+		mesh_shape.height = snap_zone_radius * 2
+		snap_zone_mesh.mesh = mesh_shape
+		snap_zone_mesh.set_surface_override_material(0,slot_material_override)
+		snap_zone_mesh.name = "MeshInstance3D"
+		add_child(snap_zone_mesh)
+		snap_zone_mesh.owner = get_tree().edited_scene_root
 
 	
 	if not Engine.is_editor_hint():
 		snap_zone = get_node("SnapZone")
-		mesh_shape_test = get_node("MeshInstance3D")
+		snap_zone_mesh = get_node("MeshInstance3D")
 		# For debugging
 		snap_zone.body_entered.connect(_body_entered_area)
 		snap_zone.body_exited.connect(_body_exited_area)
@@ -52,10 +52,12 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	if Engine.is_editor_hint():
-		snap_zone = get_node("SnapZone")
-		snap_zone.grab_distance = snap_zone_radius
-		mesh_shape_test.mesh.radius = snap_zone_radius
-		mesh_shape_test.mesh.height = snap_zone_radius * 2
+		if is_instance_valid(snap_zone):
+			snap_zone.grab_distance = snap_zone_radius
+		if is_instance_valid(snap_zone_mesh):
+			mesh_shape.radius = snap_zone_radius
+			mesh_shape.height = snap_zone_radius * 2
+			snap_zone_mesh.mesh = mesh_shape
 		
 
 func _set_current_slot_object(what) -> void:
