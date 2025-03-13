a71d278f0c61342fce712784d73bfdbfbe8c4703
Alvbatross <alvaropato54@gmail.com>
2025-03-13 14:54:32 +0800

    Fixed issue where collision masks broke for some receivers

--- addons/ateneo-virtual-reality-escape/object-interaction-system/ois-state-machine/ois-actor-states/tool_active_state.gd
+++ addons/ateneo-virtual-reality-escape/object-interaction-system/ois-state-machine/ois-actor-states/tool_active_state.gd
@@ -3,6 +3,7 @@ extends OISActorState
 
 
 func enter_state(_msg: Dictionary = {}) -> void:
+	print(_ois_actor_state_machine.get_actor_component().get_actor().name + "IS ACTIVE")
 	_ois_actor_state_machine.get_actor_component().actor_component_enabled(true)
 	_ois_actor_state_machine.get_actor_component().get_actor().released.connect(_on_actor_released)
 	_ois_actor_state_machine.get_actor_component().set_receiver(null)
@@ -12,16 +13,20 @@ func exit_state() -> void:
 
 
 func _on_enter_collision(receiver: Variant) -> void:
+	print("Entered collision")
 	if receiver.get_parent().is_in_group(_ois_actor_state_machine.get_actor_component().receiver_group):
 		if _ois_actor_state_machine.get_actor_component().get_actor() == receiver.get_parent().get_parent() and not receiver.get_parent().receive_from_self:
 			print("Actor and Receiver is the same")
 			return
 		else:
-			print("HELLO")
 			_ois_actor_state_machine.get_actor_component().set_receiver(receiver.get_parent())
 			_ois_actor_state_machine.transition_to("ActiveCollidingState", {})
 	else:
+		print("=============================================")
 		print("Incompatible Actor and Receiver")
+		print(_ois_actor_state_machine.get_actor_component().get_actor())
+		print(receiver.get_parent())
+		print("=============================================")
 
 
 func _on_actor_released(pickable: Variant, by: Variant) -> void:
